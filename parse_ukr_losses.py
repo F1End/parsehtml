aad1a990e7d9c102cbf771fd169d59ae794b9b29
F1End <horvirich@gmail.com>
2025-03-11 20:48:18 +0100

    Added wrapper for cli run of ru and ukr loss transformation

--- parse_ukr_losses.py
+++ parse_ukr_losses.py
@@ -1,3 +1,14 @@
 """
 Running loss parsing from Oryx for Ukrainian losses
 """
+from src import loss_parser
+from src import util
+
+
+if __name__ == "__main__":
+    args = util.parse_args()
+    limit, limit_tag = "Attack On Europe: Documenting Ukrainian Equipment" \
+                       " Losses During The Russian Invasion Of Ukraine", "a"
+    content = util.HTMLFileContent(args.file).load().truncate_content(limit, limit_tag)
+    ukr_losses = loss_parser.OryxLossParser().parse_losses(content())
+    util.ParsedContent(ukr_losses).load().to_csv(args.output_file)
